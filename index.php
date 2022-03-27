1f516c72686ce3c88a767654d39e31792c694b8b
ArchBacon <C.Delicaat@hotmail.com>
2022-03-27 20:26:33 +0200

    Added message for converting images

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+error_reporting(E_ALL);
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Twig\Environment;
@@ -10,7 +12,8 @@ use Twig\Loader\FilesystemLoader;
 
 $loader = new FilesystemLoader(__DIR__ . '/templates');
 $twig = new Environment($loader, [
-    'debug' => true
+    'debug' => false,
+    'strict_variables' => true,
 ]);
 $twig->addExtension(new DebugExtension());
 
@@ -21,7 +24,11 @@ foreach ($result as $image) {
     $images[] = pathinfo($image)['basename'];
 }
 
+/** @var array<int, string> $uploads */
+$uploads = glob('public/uploads/*.{jpg,jpeg,png}', GLOB_BRACE);
+
 /** @noinspection PhpUnhandledExceptionInspection */
 echo $twig->render('gallery/index.html.twig', [
     'images' => $images,
+    'hasUploads' => count($uploads) === 0,
 ]);
